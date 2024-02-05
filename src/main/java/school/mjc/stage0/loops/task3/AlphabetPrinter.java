efa864464458bb8702f945f8378be884c10d0a70
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-05 23:03:12 +0500

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+        char first = 'A';
+        char last = 'Z';
+         for (char ch = startChar; ch <= endChar; ch++) {
+            System.out.print(ch);
+        }
     }
 }
