11a8fbf3f72046e48b42716abe1e7b0c4611f204
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-05 23:08:19 +0500

    Update GreatestCommonDivisor.java

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        if(first>second){
+            int greatest=1;
+            for(int i=2; i<=second; i++){
+                if(first%i==0 && second%i==0){
+                    greatest=i;
+                }
+            }
+            System.out.println(greatest);
+        }
+        else{
+            int greatest=1;
+            for(int i=2; i<=first; i++){
+                if(first%i==0 && second%i==0){
+                    greatest=i;
+                }
+            }
+            System.out.println(greatest);
+        }
     }
 }
