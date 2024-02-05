5a73d3ac279c556fb6fd57427ed4fc7f519e33b7
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-05 23:24:21 +0500

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+        String numberStr = number + "";
+        
+        int sum = 0;
 
+        for (int i = 0; i < numberStr.length(); i++) {
+            int digit = Character.getNumericValue(numberStr.charAt(i));
+            sum += digit;
+        }
+
+        System.out.println(sum);
     }
 }
